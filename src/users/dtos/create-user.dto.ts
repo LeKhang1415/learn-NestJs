9573af20957b6d87c5f4f959a30297ecbfa77e33
khang <chaoskid1415@gmail.com>
2025-03-12 17:23:46 +0700

    Tao userModule

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUser {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  lastName?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^(?=.*d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/, {
+    message: 'Invalid password',
+  })
+  password: string;
+}
